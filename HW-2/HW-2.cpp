7427148590af5c8a52caffdf0c3447e821f56901
Olga Loseva <160180819+LeahLeah88@users.noreply.github.com>
2025-05-11 21:20:42 +0300

    Add project files.

---
+++ HW-2/HW-2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+int Swap(int a)
+{
+	if (a > 9 && a < 100)
+	{
+		int c1;
+		int c2;
+		c1 = a / 10;
+		c2 = a % 10;
+		return c2 * 10 + c1;
+	}
+	else
+		return a;
+}
+
+int main()
+{
+	int c;
+	std::cout << "c = ";
+	std::cin >> c;
+	std::cout << Swap(c);
