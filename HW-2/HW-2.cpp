3aeb99f28a8456674e58ca673f0ab5b09f2a1174
Olga Loseva <160180819+LeahLeah88@users.noreply.github.com>
2025-09-23 20:02:06 +0300

    Объединены объявление и инициализация переменных

--- HW-2/HW-2.cpp
+++ HW-2/HW-2.cpp
@@ -4,10 +4,8 @@ int swap(int number)
 {
 	if (number > 9 && number < 100)
 	{
-		int figure1;
-		int figure2;
-		figure1 = number / 10;
-		figure2 = number % 10;
+		int figure1 = number / 10;
+		int figure2 = number % 10;
 		return figure2 * 10 + figure1;
 	}
 	else
@@ -21,3 +19,4 @@ int main()
 	std::cin >> number;
 
 	std::cout << Swap(number);
+
